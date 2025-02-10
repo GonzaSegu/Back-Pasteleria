d5bf3967d94b06b85c33e5b001cbe2bd5e23a31d
Gonzalo Seguel <gonzalo.seguel@gsti.cl>
2025-02-10 14:36:41 -0300

    agregué permisos para productos

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,12 +1,18 @@
 const {Router} = require('express')
 const ProductosController  = require('../controllers/productos.controller')
+const {authMiddleware, verifyRole } = require('../middlewares/authMiddleware')
 
 const router = Router()
 
-router.post("/", ProductosController.handleCreateProduct)      //Create producto
+// 🔒 Solo administradores pueden crear productos
+router.post("/", authMiddleware, verifyRole([1]), ProductosController.handleCreateProduct)      //Create producto
+
+// 🟢 Cualquier usuario puede ver los productos
 router.get("/", ProductosController.handleReadProducts)        //Read productos
 router.get("/:id", ProductosController.handleReadProduct)      //Read producto por id
-router.put("/:id", ProductosController.handleUpdateProduct)    //Update producto
-router.delete("/:id", ProductosController.handleDeleteProduct) //Delete producto
+
+// 🔒 Solo administradores pueden actualizar o eliminar productos
+router.put("/:id", authMiddleware, verifyRole([1]), ProductosController.handleUpdateProduct)    //Update producto
+router.delete("/:id", authMiddleware, verifyRole([1]), ProductosController.handleDeleteProduct) //Delete producto
 
 module.exports = router;
